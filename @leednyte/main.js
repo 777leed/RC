ca9ba47f23d683bb0dba8f035a6094b4dc7612bd
Khalid koukou <86270022+777leed@users.noreply.github.com>
2023-09-05 16:38:09 +0100

    projects page demo 1.2

---
+++ @leednyte/main.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp(
+    {
+        data() {
+            return {
+                projects:[  
+                {id:"p1",img:"../Assets/Projects pics/tassdert project.webp",title:"Well/Solar Panel Additions in Tassdert, Morocco"},
+                {id:"p2",img:"../Assets/Projects pics/Life Skills Camp.webp",title:"Life Skills Camp in Foum Jemaa, Morocco"},
+                {id:"p3",img:"../Assets/Projects pics/Neighborhood _Zwin-Up” Activity 5.webp",title:"Zwin-Up Activity in Tameslouht, Morocco"},
+                {id:"p4",img:"../Assets/Projects pics/Dual-Powered Kiln Prototype Dimensions and Interior.webp",title:"Eco-Friendly Pottery Kiln Prototypes"},
+                {id:"p5",img:"../Assets/Projects pics/Sawdust Briquettes.webp",title:"Materials Testing of Combustible Bio-Bricks for the Pottery Kilns"},
+                {id:"p6",img:"../Assets/Projects pics/Methane Gas Capping System.webp",title:"Measuring the Calorific Value of Methane Gas from the Wastewater Treatment Facility"},
+                {id:"p7",img:"../Assets/Projects pics/Moroccan Organic Soap.webp",title:"Processing of Olive Oil Wastewater into Organic Soap"},
+                {id:"p8",img:"../Assets/Projects pics/Neighborhood _Zwin-Up” Activity 15.webp",title:"Zwin-Up Activities in Tameslouht, Morocco"},
+                {id:"p9",img:"../Assets/Projects pics/20170523_123410.webp",title:"Playground in Tameslouht, Morocco"},
+                {id:"p10",img:"../Assets/Projects pics/CD Activities 1.webp",title:"Camp Day in Agoundis Valley!"},
+                {id:"p11",img:"../Assets/Projects pics/Aguerda Kids.webp",title:"Rehab Construction & Equipping of Kindergarten in Aguerda, Morocco"},
+                {id:"p12",img:"../Assets/Projects pics/Business Dev Workshop 2_Selecting the Board.webp",title:"Women's Empowerment Workshops & Classroom in Aguerda, Morocco"}
+            
+            ]
+            }
+        }
+    }
+
+)
